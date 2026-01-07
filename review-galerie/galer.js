2ea0eaa48791df5fe70dbeb38128404f04f37bac
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-01-07 20:26:12 +0100

    challenge galerie review - 80% done - the showPopular function must be repaird

--- review-galerie/galer.js
+++ review-galerie/galer.js
@@ -1,77 +1,69 @@
+const photos = [
+  { id: 1, title: "Sonnenuntergang", likes: 42, emoji: "☀️" },
+  { id: 2, title: "Berge", likes: 38, emoji: "⛰️" },
+  { id: 3, title: "Strand", likes: 55, emoji: "🐚" },
+  { id: 4, title: "Stadt", likes: 29, emoji: "🌆" },
+];
+
 function init() {
-    renderGallery();
-    el();
-  }
-  
-  function renderGallery(customPhotos = photos) {
-    const x = document.getElementById("gallery");
-    x.innerHTML = "";
-  
-    customPhotos.forEach((photo) => {
-      x.innerHTML += createPhotoCard(photo);
-    });
-  }
-  
-  photos = [
-    { id: 1, title: "Sonnenuntergang", likes: 42 },
-    { id: 2, title: "Berge", likes: 38 },
-    { id: 3, title: "Strand", likes: 55 },
-    { id: 4, title: "Stadt", likes: 29 },
-  ];
-  
-  function createPhotoCard(photo) {
-    const x = document.getElementById("gallery");
-    x.innerHTML += `
-      <div class="photo-placeholder">{photo.emoji}</div><div class="photo-info"><div class="photo-title">${photo.title}</div>
-                  <div class="photo-likes">❤️ ${photo.likes} Likes</div>
-      <button class="like-btn" onclick="likePhoto(${photo.id})">Like</button></div>
-              `;
-  }
-  
-  function likePhoto() {
-    const photo = photos.find((p) => p.id === id);
-  
-    if (photo) {
-      photo.likes++;
-      renderGallery();
-    }
-  }
-  function el() {
-    document
-      .getElementById("addPhotoBtn")
-      .addEventListener("onclick", addNewPhoto);
-  
-    document
-      .getElementById("resetBtn")
-      .addEventListener("click", () => renderGallery());
-  }
-  
-  function addNewPhoto() {
-    let titleInput = document.getElementById("photoTitle");
-    const emojiInput = document.getElementById("photoEmoji");
-  
-    const title = titleInput.value;
-    const emoji = emojiInput.value;
-  
-    if ((title = "" || emoji == "")) alert("Bitte fülle beide Felder aus!");
-  
-    newPhoto = {
-      id: 4,
-      title: title,
-      emoji: emoji,
-      likes: 0,
-    };
-  
-    photos.push(newPhoto);
-  
-    titleInput.value = "";
-    emojiInput.value = "";
-  }
-  
   renderGallery();
-  
-  function showPopular() {
-    const popular = photos.filter((photo = photo.likes > 40));
-    renderGallery(popular);
+}
+
+function renderGallery(customPhotos = photos) {
+  const galleryRef = document.getElementById("gallery");
+  galleryRef.innerHTML = "";
+  customPhotos.forEach((photo) => {
+    galleryRef.innerHTML += templetPhotoCard(photo);
+  });
+  console.log(photos);
+}
+
+function templetPhotoCard(photo) {
+  return `
+          <div class="photo-placeholder">${photo.emoji}</div>
+          <div class="photo-info">
+            <div class="photo-title">${photo.title}</div>
+            <div class="photo-likes">❤️ ${photo.likes} Likes</div>
+            <button class="like-btn" onclick="likePhoto(${photo.id})">Like</button>
+          </div>`;
+}
+
+function likePhoto(likeId) {
+  const photo = photos.find((index) => index.id == likeId);
+  if (photo) {
+    photo.likes++;
+    renderGallery();
   }
-  
+}
+
+function addNewPhoto() {
+  let titleInput = document.getElementById("photoTitle");
+  let emojiInput = document.getElementById("photoEmoji");
+
+  let title = titleInput.value;
+  let emoji = emojiInput.value;
+
+  if (title == "" || emoji == "") {
+    alert("Bitte fülle beide Felder aus!");
+    return;
+  }
+
+
+  const newPhoto = {
+    id: (photos.length + 1),
+    title: title,
+    likes: 20,
+    emoji: emoji,
+  };
+  console.log(photos + "new Photo: " + newPhoto)
+  photos.push(newPhoto);
+
+  titleInput.value = "";
+  emojiInput.value = "";
+  renderGallery();
+}
+
+function showPopular() {
+  const popular = photos.filter((photo = photo.likes > 40));
+  renderGallery(popular);
+}
